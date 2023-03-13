8d5bfe8c6a59b08583cee20178f8a70bc447ec14
Sam Gillies <sam@samgillies.com>
2023-03-13 14:30:31 +0000

    page text now generates via JS correctly

--- src/index.js
+++ src/index.js
@@ -1,61 +1,76 @@
-console.log('JS is working correctly'); 
+console.log('JS is loading correctly'); 
 
 function component() {
-    const element = document.createElement('div');
-    element.setAttribute('id','content');
 
-    const header = element.createElement('section'); 
-    header.setAttribute('id','header');
+    const element = document.getElementById('content'); 
 
-    const titles = header.createElement('div'); 
+    const header = document.createElement('section'); 
+    header.setAttribute('id','header'); 
+    element.appendChild(header); 
+
+    const titles = document.createElement('div'); 
+    header.appendChild(titles); 
     titles.setAttribute('id','header--titles');
 
-    const titleMain = titles.createElement('h1'); 
+    const titleMain = document.createElement('h1'); 
+    titles.appendChild(titleMain); 
     titleMain.setAttribute('id','header--main');
     titleMain.innerHTML = 'MED-ONE'; 
 
-    const titleSub = titles.createElement('h1'); 
+    const titleSub = document.createElement('h1'); 
+    titles.appendChild(titleSub); 
     titleSub.setAttribute('id','header--sub');
     titleSub.innerHTML = 'Mediterranean Cuisine in West Yorkshire'; 
 
-    const links = header.createElement('div'); 
+    const links = document.createElement('div'); 
+    header.appendChild(links); 
     links.setAttribute('id','header--links');
 
-    const list = links.createElement('ul'); 
+    const list = document.createElement('ul');
+    links.appendChild(list);  
     list.setAttribute('id','header--nav');
 
-    const listItem1 = links.createElement('li'); 
+    const listItem1 = document.createElement('li'); 
+    list.appendChild(listItem1); 
     listItem1.setAttribute('id','header--nav1');
     listItem1.innerHTML = 'About';
 
-    const listItem2 = links.createElement('li'); 
+    const listItem2 = document.createElement('li');
+    list.appendChild(listItem2); 
     listItem2.setAttribute('id','header--nav2');
     listItem2.innerHTML = 'Menu';
 
-    const listItem3 = links.createElement('li'); 
+    const listItem3 = document.createElement('li'); 
+    list.appendChild(listItem3); 
     listItem3.setAttribute('id','header--nav3');
     listItem3.innerHTML = 'Contact';
     
-    const about = element.createElement('section'); 
+    const about = document.createElement('section'); 
+    element.appendChild(about); 
     about.setAttribute('id','about');
 
-    const aboutContent = about.createElement('div'); 
+    const aboutContent = document.createElement('div'); 
+    about.appendChild(aboutContent); 
     aboutContent.setAttribute('id','about--content');
 
-    const aboutTitle = aboutContent.createElement('h1'); 
+    const aboutTitle = document.createElement('h1'); 
+    aboutContent.appendChild(aboutTitle); 
     aboutTitle.setAttribute('id','about--main');
     aboutTitle.innerHTML = 'About';
 
-    const aboutInfoBlock = aboutContent.createElement('div'); 
+    const aboutInfoBlock = document.createElement('div'); 
+    aboutContent.appendChild(aboutInfoBlock); 
     aboutInfoBlock.setAttribute('id','about--infoblock');
 
-    const aboutImage = aboutInfoBlock.createElement('img'); 
+    const aboutImage = document.createElement('img');
+    aboutInfoBlock.appendChild(aboutImage);  
     aboutImage.setAttribute('id','about--image');
     aboutImage.setAttribute('src','../src/med-one1.png');
 
-    const aboutText = aboutInfoBlock.createElement('p'); 
+    const aboutText = document.createElement('p'); 
+    aboutInfoBlock.appendChild(aboutText); 
     aboutText.setAttribute('id','about--text');
-    aboutTitle.innerHTML = 'Huddersfield\'s most beloved place to eat. Chef Khalil takes pride in creating beautiful, fresh and tasty food for the people of this town. They have earned TripAdvisor’s Certificate of Excellence for being an outstanding business 5 years in a row. <br> + <br> They have a wonderful staff team whose aim is to make every visit a perfect one.';
+    aboutText.innerHTML = 'Huddersfield\'s most beloved place to eat. Chef Khalil takes pride in creating beautiful, fresh and tasty food for the people of this town. They have earned TripAdvisor’s Certificate of Excellence for being an outstanding business 5 years in a row. <br> <br> They have a wonderful staff team whose aim is to make every visit a perfect one.';
 
     return element;
   }
